a0340c1becae588a94043111f4e0e61b97f996bb
saraivamatheus <132276931+saraivamatheus@users.noreply.github.com>
2024-03-20 20:50:34 -0300

    a

--- aula11/ex008.js
+++
@@ -1,8 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}KM/H`)
-if (vel > 60){
-    console.log('Você ultrapassou a velocidade permitida. Multado!')
-} else{
-    console.log('Jeguilson Milson')
-}
-console.log('Dirija sempre usando cinto de segurança')
